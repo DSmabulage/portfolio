4eaf50239410d3de3ab293ab7b4d98d0cea3d41d
dileepa inivossl <dileepa.mabulage@inivossl.com>
2022-07-03 08:09:01 +0530

    init

---
+++ script.js
@@ -0,0 +1,81 @@
+var readmorebtn1 = document.querySelector("#r-btn-1");
+const text1 = document.querySelector("#c-t-1");
+
+
+readmorebtn1.addEventListener('click',(e)=>{
+    text1.classList.toggle('show-more');
+
+    if(readmorebtn1.innerText ===  'Read More'){
+        readmorebtn1.innerText = 'Read Less';
+    }
+    else{  
+        readmorebtn1.innerText = 'Read More'
+    }
+})
+
+
+
+
+
+
+
+
+
+var readmorebtn2 = document.querySelector("#r-btn-2");
+const text2 = document.querySelector("#c-t-2");
+
+
+readmorebtn2.addEventListener('click',(e)=>{
+    text2.classList.toggle('show-more');
+
+    if(readmorebtn2.innerText ===  'Read More'){
+        readmorebtn2.innerText = 'Read Less';
+    }
+    else{  
+        readmorebtn2.innerText = 'Read More'
+    }
+})
+
+
+
+
+
+
+var readmorebtn3 = document.querySelector("#r-btn-3");
+const text3 = document.querySelector("#c-t-3");
+
+
+readmorebtn3.addEventListener('click',(e)=>{
+    text3.classList.toggle('show-more');
+
+    if(readmorebtn3.innerText ===  'Read More'){
+        readmorebtn3.innerText = 'Read Less';
+    }
+    else{  
+        readmorebtn3.innerText = 'Read More'
+    }
+})
+
+
+
+
+
+
+
+var readmorebtn4 = document.querySelector("#r-btn-4");
+const text4 = document.querySelector("#c-t-4");
+
+
+readmorebtn4.addEventListener('click',(e)=>{
+    text4.classList.toggle('show-more');
+
+    if(readmorebtn4.innerText ===  'Read More'){
+        readmorebtn4.innerText = 'Read Less';
+    }
+    else{  
+        readmorebtn4.innerText = 'Read More'
+    }
+})
+
+
+
